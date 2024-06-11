70006709b41d1e1ccc2d524a8e9e588e64ec6068
Mo-elaissati <mohamed.elaissati@kamerksem.be>
2024-06-11 18:29:28 +0200

    Add files via upload

---
+++ main - amina.py
@@ -0,0 +1,21 @@
+# Oefening 1
+mt = ["ontbijt","lunch","diner","nachtmaal"]
+print(mt[0:2])
+
+#Oefening 2
+ft = ["appel","banaan","kers","druif","mango"]
+print(ft[-2:])
+
+#Oefening 3
+hb = ["lezen","schaken","wandelen","fietsen"]
+print(hb[:4])
+
+#Oefening4
+kl = ["rood","groen","blauw","geel","paars"]
+print(kl[2:])
+
+#Oefening 5
+hs = ["brussel","amsterdam","berlijn","madrid","rome"]
+print("hier zijn de eerste 3 hoofdsteden:")
+for ht in hs[:3]:
+  print(ht.title())
